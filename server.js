f22841d487f959a9e0e1569fb2ad22c049bfe7d9
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-28 06:35:11 -0400

    added the app.use, which sets up the Express app to handle data parsing

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ var bothParser = require ("body-parser");
 //Sets up the express app
 var app = express ();
 var Port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
