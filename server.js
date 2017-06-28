331e743b79395dc3ac38ed096315e8cb739e0227
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-28 06:32:25 -0400

    added the dependencies and app and Port to set up express app

---
+++ server.js
@@ -0,0 +1,8 @@
+//These are the Dependencies
+var express = require ("express");
+var path = require ("path");
+var bothParser = require ("body-parser");
+
+//Sets up the express app
+var app = express ();
+var Port = 3000;
