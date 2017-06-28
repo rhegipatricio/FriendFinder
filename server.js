21a270f6415484849d2309159268336287307872
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-28 06:37:11 -0400

    added listener

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ var bothParser = require ("body-parser");
 
 //Sets up the express app
 var app = express ();
-var Port = 3000;
+var PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+require('/app/routing/apiRoutes.js')(app);
+require('/app/routing/htmlRoutes.js')(app);
+
+app.listen(Port, function () {
+	console.log("App listening on PORT: " + PORT)
+
+});
