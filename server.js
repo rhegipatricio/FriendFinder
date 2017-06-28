5dc453193bbc64fd875f8e9aa977a9ea37ed7216
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-28 09:51:20 -0400

    found spelling error on server.js and fixed from bothParcer to bodyParcer

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //These are the Dependencies
 var express = require ("express");
 var path = require ("path");
-var bothParser = require ("body-parser");
+var bodyParser = require ("body-parser");
 
 //Sets up the express app
 var app = express ();
