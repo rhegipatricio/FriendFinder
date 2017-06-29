997a07b7bb2463ed58b5551997a464f9caed348d
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-28 23:50:18 -0400

    changed some questions on survey.html and added answers for friends on friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,162 +1,162 @@
 var friends = [
 	{
 		name: "Amber Garcia",
-		photo: "",
+		photo: "http://i31.photobucket.com/albums/c371/gindawg2k/ag.jpg",
 		scores: [
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-
+				2,
+				4,
+				3,
+				4,
+				2,
+				5,
+				2,
+				5,
+				3,
+				1
 		]
 	},
 	{
 		name: "Sam Newton",
-		photo: "",
+		photo: "http://i31.photobucket.com/albums/c371/gindawg2k/sn.jpg",
 		scores: [
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-
+				2,
+				1,
+				1,
+				1,
+				2,
+				1,
+				4,
+				3,
+				3,
+				1
 		]
 	},
 	{
 		name: "Marisa Vela",
-		photo: "",
+		photo: "http://i31.photobucket.com/albums/c371/gindawg2k/mv.jpg",
 		scores: [
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-
+				2,
+				3,
+				1,
+				2,
+				4,
+				1,
+				4,
+				1,
+				1,
+				2
 		]
 	},
 	{
 		name: "Ashley Garcia",
-		photo: "",
+		photo: "http://i31.photobucket.com/albums/c371/gindawg2k/mg.jpg",
 		scores: [
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-
+				2,
+				5,
+				4,
+				4,
+				2,
+				4,
+				2,
+				5,
+				4,
+				4
 		]
 	},
 	{
 		name: "Rhiannon Sanders",
-		photo: "",
+		photo: "http://i31.photobucket.com/albums/c371/gindawg2k/rs.jpg",
 		scores: [
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-
+				3,
+				3,
+				1,
+				4,
+				1,
+				2,
+				3,
+				5,
+				2,
+				1
 		]
 	},
 	{
 		name: "Cody Morgan",
-		photo: "",
+		photo: "http://i31.photobucket.com/albums/c371/gindawg2k/rsz_cm.jpg",
 		scores: [
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-
+				2,
+				2,
+				3,
+				2,
+				3,
+				2,
+				4,
+				3,
+				2,
+				1
 		]
 	},
 	{
 		name: "Vanessa Gable",
-		photo: "",
+		photo: "http://i31.photobucket.com/albums/c371/gindawg2k/vg.jpg",
 		scores: [
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-
+				4,
+				4,
+				2,
+				3,
+				1,
+				4,
+				3,
+				5,
+				2,
+				1
 		]
 	},
 	{
 		name: "Nicholas Nobriga",
-		photo: "",
+		photo: "http://i31.photobucket.com/albums/c371/gindawg2k/nn.jpg",
 		scores: [
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-
+				1,
+				2,
+				4,
+				3,
+				1,
+				5,
+				1,
+				5,
+				5,
+				4
 		]
 	},
 	{
 		name: "Shannon Cruz",
-		photo: "",
+		photo: "http://i31.photobucket.com/albums/c371/gindawg2k/sc.jpg",
 		scores: [
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-
+				4,
+				4,
+				1,
+				1,
+				3,
+				4,
+				2,
+				4,
+				3,
+				2
 		]
 	},
 	{
 		name: "Lendell Ray",
-		photo: "",
+		photo: "http://i31.photobucket.com/albums/c371/gindawg2k/lr.jpg",
 		scores: [
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-				,
-
+				4,
+				4,
+				2,
+				4,
+				1,
+				3,
+				2,
+				5,
+				2,
+				1
 		]
 	}
 
